0edccbc86798e36901a71b7ef7a3267c654c181b
Kristina Martins <44987714+zakharovakr@users.noreply.github.com>
2024-05-24 12:19:01 -0400

    Update interest_rate_tests.cy.js
    improved the code

--- cypress/e2e/interest_rate_tests.cy.js
+++ cypress/e2e/interest_rate_tests.cy.js
@@ -4,36 +4,48 @@ describe('Interest Rate Input Field Scenarios', () => {
     beforeEach(() => {
         // Go to the Zillow Mortgage Calculator page before running each case
         cy.visit('https://www.zillow.com/mortgage-calculator/')
-      
-      })
+
+    })
 
     it('Test 1 - Valid input (integer)', () => {
 
-        // Get the default value for "Your payment" and set it to a variable
-        const your_payment_default = cy.get('[y="20"]')
-        // Find the interest rate field, clear the prepopulated value and type a new valid one 
-        cy.get('#rate')
-            .clear()
-            .type('50')
-        // Click away from the input field
-        cy.get('body').click(0,0);
-        // Get the updated value for "Your payment" and set it to a new variable
-        const your_payment_updated = cy.get('[y="20"]')
-        // Validate that the value for "Your Payment" has changed
-        expect(your_payment_default).not.equal(your_payment_updated)
+        // Get the default text value for "Your payment" 
+        cy.get('text[y="20"]')
+            .invoke('text')
+            .then((your_payment_default) => {
+
+                //Change the default interest rate value to 50
+                cy.get('#rate')
+                    .clear()
+                    .type('50')
+                // Click away from the input field and wait for 1 sec till page fully loads
+                cy.get('body').click(0, 0)
+                cy.wait(1000)
+
+                //Get the text value for "Your payment" again and compare it to the previous value
+                //The values should not match
+                cy.get('text[y="20"]')
+                    .invoke('text')
+                    .should((your_payment_updated) => {
+                        expect(your_payment_default).not.to.eq(your_payment_updated)
+                    }
+                    )
+
+            })
     })
 
     it('Test 2 - Invalid input (letters)', () => {
 
-         // Find the interest rate field, clear the prepopulated value and type letters instead of numbers 
-         cy.get('#rate')
+        // Find the interest rate field, clear the prepopulated value and type letters instead of numbers 
+        cy.get('#rate')
             .clear()
             .type('abc')
-         // Click away from the input field
-         cy.get('body').click(0,0);
-         // Validate the error message appears and has the expected text value
-         cy.get('p[id^=__c11n_]')
+        // Click away from the input field and wait for 1 sec
+        cy.get('body').click(0, 0)
+        cy.wait(1000)
+        // Validate the error message appears and has the expected text value
+        cy.get('p[id^=__c11n_]')
             .should('be.visible')
             .and("have.text", "'abc' is not a valid number")
     })
-  })
+})
