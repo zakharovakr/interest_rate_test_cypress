dad3bee395f23ebeafb65889042b1d09c21429e0
Kristina Martins <44987714+zakharovakr@users.noreply.github.com>
2024-06-06 00:25:34 -0400

    Update interest_rate_tests.cy.js
    added cy.intercept() instead of cy.wait() for test 1 to make sure page fully loads before I compare "your payment" text values

--- cypress/e2e/interest_rate_tests.cy.js
+++ cypress/e2e/interest_rate_tests.cy.js
@@ -9,6 +9,9 @@ describe('Interest Rate Input Field Scenarios', () => {
 
     it('Test 1 - Valid input (integer)', () => {
 
+        // Intercept the API call that updates your payment information
+        cy.intercept('POST', '/api/v2/collector').as('updatePayment')
+
         // Get the default text value for "Your payment" 
         cy.get('text[y="20"]')
             .invoke('text')
@@ -18,13 +21,15 @@ describe('Interest Rate Input Field Scenarios', () => {
                 cy.get('#rate')
                     .clear()
                     .type('50')
-                // Click away from the input field and wait for 1 sec till page fully loads
+                // Click away from the input field
                 cy.get('body').click(0, 0)
-                cy.wait(1000)
+
+                // Wait for both API calls to complete (page fully loads)
+                cy.wait(['@updatePayment', '@updatePayment'])
 
                 //Get the text value for "Your payment" again and compare it to the previous value
                 //The values should not match
-                cy.get('text[y="20"]')
+                cy.get('text[y="20"]', { timeout: 20000 })
                     .invoke('text')
                     .should((your_payment_updated) => {
                         expect(your_payment_default).not.to.eq(your_payment_updated)
@@ -42,7 +47,7 @@ describe('Interest Rate Input Field Scenarios', () => {
             .type('abc')
         // Click away from the input field and wait for 1 sec
         cy.get('body').click(0, 0)
-        cy.wait(1000)
+        
         // Validate the error message appears and has the expected text value
         cy.get('p[id^=__c11n_]')
             .should('be.visible')
